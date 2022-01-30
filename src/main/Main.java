c7998f889a55a1308231a4a93f89c103750f5e01
RaMcDaniel <rmcda35@wgu.edu>
2022-01-30 00:32:44 -0500

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/** Main the main class, and entry to the program.
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+
+    /** This is the main method that runs when the program launches.
+     * Launch tells JavaFX to take over and launch a window.
+     * @param args Currently args is just a placeholder.
+     */
+    public static void main(String[] args){
+       launch(args);
+    }
+}
