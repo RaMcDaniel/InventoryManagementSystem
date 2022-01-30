0bea21a2409c64ac99f7131dd0f2a4ed1ef4dba7
RaMcDaniel <rmcda35@wgu.edu>
2022-01-30 02:01:00 -0500

    Main screen layout created

---
+++ src/controller/MainScreen.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainScreen implements Initializable {
+    /** This method is auto-created by extending Initializable.
+     * @param url PENDING
+     * @param resourceBundle PENDING
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("I am initialized");
+
+    }
+}
